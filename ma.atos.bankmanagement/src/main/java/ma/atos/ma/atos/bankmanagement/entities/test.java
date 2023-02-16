30fda59b3cea118e254c7ea6aca77bf6e15a0d6e
ikram Berradi <ikram20berradi@gmail.com>
2023-02-16 23:00:12 +0100

    ikram berradi

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/entities/test.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/entities/test.java
@@ -1,7 +1,8 @@
 package ma.atos.ma.atos.bankmanagement.entities;
 
 import java.util.Date;
-public  abstract class Tier {
+
+public  abstract class test {
      private Long idClient;
     private Long idClient;
     private String nationalite;
