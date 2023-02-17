010af376e29be410a84226036a5d8e1bf43cd7d8
soukaina <bouchane.soukaina1@gmail.com>
2023-02-17 10:43:54 +0100

    Add Agence and Compte class

---
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/entities/Agence.java
@@ -0,0 +1,11 @@
+package ma.atos.ma.atos.bankmanagement.entities;
+
+import lombok.Data;
+
+@Data
+public class Agence {
+
+    private Long idAgence;
+    private String nomAgence;
+    private String adressAgence;
+}
---
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/entities/Compte.java
@@ -0,0 +1,15 @@
+package ma.atos.ma.atos.bankmanagement.entities;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Compte {
+
+    private Long idCompte;
+    private Long ribCompte;
+    private Double balance;
+    private Date dateCreation;
+    private String devise;
+}
