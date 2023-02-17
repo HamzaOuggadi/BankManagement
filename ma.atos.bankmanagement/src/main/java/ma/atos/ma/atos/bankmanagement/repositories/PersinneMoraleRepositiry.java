9c1f8e41f42ffafc0a26ba876d870da79508d7f9
ikram Berradi <ikram20berradi@gmail.com>
2023-02-17 15:49:59 +0100

    ikram berradi

---
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/repositories/PersinneMoraleRepositiry.java
@@ -0,0 +1,7 @@
+package ma.atos.ma.atos.bankmanagement.repositories;
+
+import ma.atos.ma.atos.bankmanagement.entities.PersonneMorale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersinneMoraleRepositiry extends JpaRepository<PersonneMorale ,Long   > {
+}
