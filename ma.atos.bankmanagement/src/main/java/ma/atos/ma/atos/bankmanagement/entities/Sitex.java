34be6957393614f37bd90edf313308dcf1dd4d67
HamzaOuggadi <hamza.ouggadi@gmail.com>
2023-02-17 14:34:49 +0100

    Added Sitex.java

---
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/entities/Sitex.java
@@ -0,0 +1,13 @@
+package ma.atos.ma.atos.bankmanagement.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sitex {
+    private Long idSitex;
+    private String type;
+}
