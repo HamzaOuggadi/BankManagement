78c5789304d92b2ccde6c57ccb57b6457a4cc68f
soukaina <bouchane.soukaina1@gmail.com>
2023-02-18 21:13:05 +0100

    Added Mappers

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/mappers/CompteMapper.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/mappers/CompteMapper.java
@@ -4,7 +4,9 @@ import ma.atos.ma.atos.bankmanagement.Dtos.AgenceDto;
 import ma.atos.ma.atos.bankmanagement.Dtos.CompteDto;
 import ma.atos.ma.atos.bankmanagement.entities.Agence;
 import ma.atos.ma.atos.bankmanagement.entities.Compte;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface CompteMapper {
 
     CompteDto compteToCompteDto(Compte compte);
--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/mappers/GestionnaireMapper.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/mappers/GestionnaireMapper.java
@@ -1,7 +1,15 @@
 package ma.atos.ma.atos.bankmanagement.mappers;
 
+import ma.atos.ma.atos.bankmanagement.Dtos.CompteDto;
+import ma.atos.ma.atos.bankmanagement.Dtos.GestionnaireDto;
+import ma.atos.ma.atos.bankmanagement.entities.Compte;
+import ma.atos.ma.atos.bankmanagement.entities.Gestionnaire;
+import org.mapstruct.Mapper;
+
+
+@Mapper
 public interface GestionnaireMapper {
 
-
+    GestionnaireDto gestionnaireToGestionnaireDto(Gestionnaire gestionnaire);
 
 }
