6f1fb7ef55ec9a1b01342679ab96faf595df885d
a899431 <najwa.elinani@atos.net>
2023-02-21 17:19:15 +0100

    Merge remote-tracking branch 'origin/main'

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/services/RestrictionService.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/services/RestrictionService.java
@@ -1,12 +1,14 @@
 package ma.atos.ma.atos.bankmanagement.services;
 
+import ma.atos.ma.atos.bankmanagement.Dtos.RestrictionDto;
 import ma.atos.ma.atos.bankmanagement.entities.Restriction;
+import ma.atos.ma.atos.bankmanagement.exceptions.RestrictionException;
 
 import java.util.List;
 
 public interface RestrictionService {
-    List<Restriction> getResctriction();
+    List<RestrictionDto> getResctriction();
     Restriction getRestricionById(Long idRestriction);
-    Restriction createRestriction( Restriction restriction);
-    void deleteRestriction(Long idRestriction);
+    void createRestriction( RestrictionDto restrictionDto);
+    void deleteRestriction(Long idRestriction) throws RestrictionException ;
 }
