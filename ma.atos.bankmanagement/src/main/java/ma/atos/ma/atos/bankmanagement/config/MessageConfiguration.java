afae6a43ae909ae05607c0675ca4c26d6919649f
a899431 <najwa.elinani@atos.net>
2023-02-22 16:25:06 +0100

    Merge remote-tracking branch 'origin/main'

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/config/MessageConfiguration.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/config/MessageConfiguration.java
@@ -14,5 +14,6 @@ public class MessageConfiguration {
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+
     }
 }
