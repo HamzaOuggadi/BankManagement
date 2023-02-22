0c29917e70d26523da2e81bfdad447a938af69e3
a899431 <najwa.elinani@atos.net>
2023-02-22 23:17:44 +0100

    Merge remote-tracking branch 'origin/main'

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/exceptions/RestrictionException.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/exceptions/RestrictionException.java
@@ -1,8 +1,29 @@
 package ma.atos.ma.atos.bankmanagement.exceptions;
 
-public class RestrictionException extends Exception{
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
-    public RestrictionException(String message) {
-        super(message);
+
+@Setter
+public class RestrictionException extends RuntimeException{
+    private String message;
+    private String code;
+    private HttpStatus httpStatus;
+
+    public RestrictionException(String message){
+        super();
+        this.message=message;
     }
-}
+    public RestrictionException(String message,HttpStatus httpStatus){
+        super();
+        this.message=message;
+        this.httpStatus= httpStatus;
+    }
+    public RestrictionException(String message, String code, HttpStatus httpStatus){
+        super();
+        this.message=message;
+        this.code=code;
+        this.httpStatus= httpStatus;
+    }
+    }
+
--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/services/RestrictionServiceImpl.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/services/RestrictionServiceImpl.java
@@ -2,22 +2,30 @@ package ma.atos.ma.atos.bankmanagement.services;
 
 import lombok.AllArgsConstructor;
 import ma.atos.ma.atos.bankmanagement.Dtos.RestrictionDto;
+import ma.atos.ma.atos.bankmanagement.config.MessageConfiguration;
 import ma.atos.ma.atos.bankmanagement.entities.Restriction;
 import ma.atos.ma.atos.bankmanagement.exceptions.RestrictionException;
 import ma.atos.ma.atos.bankmanagement.mappers.RestrictionMapper;
 import ma.atos.ma.atos.bankmanagement.repositories.RestrictionRepository;
+import org.aspectj.weaver.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.scanner.Constant;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Locale;
+
 
 @Service
 @AllArgsConstructor
 public class RestrictionServiceImpl implements RestrictionService {
     RestrictionRepository restrictionRepo;
     RestrictionMapper restrictionMapper;
+    MessageSource messageSource;
+
 
     @Override
     public List<RestrictionDto> getResctriction() {
@@ -40,7 +48,9 @@ public class RestrictionServiceImpl implements RestrictionService {
     @Override
     public void deleteRestriction(Long idRestriction) throws RestrictionException {
         if (restrictionRepo.findById(idRestriction)== null){
-            throw new RestrictionException("Restriction already deleted");
+            throw new RestrictionException(
+                    messageSource.getMessage("idrestriction.empty", new Object[]{}, Locale.getDefault())
+                            , HttpStatus.BAD_REQUEST);
         }
         else{
             restrictionRepo.deleteById(idRestriction);
