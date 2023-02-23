5552238c42afd76846163af7ffb78f1bb241d9fa
HamzaOuggadi <hamza.ouggadi@gmail.com>
2023-02-23 16:05:57 +0100

    Ajout du sens inverse pour les mappers

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/Application.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/Application.java
@@ -1,5 +1,6 @@
 package ma.atos.ma.atos.bankmanagement;
 
+import lombok.extern.slf4j.Slf4j;
 import ma.atos.ma.atos.bankmanagement.Dtos.CompteDto;
 import ma.atos.ma.atos.bankmanagement.Dtos.OperationDto;
 import ma.atos.ma.atos.bankmanagement.entities.Operation;
@@ -15,49 +16,53 @@ import org.springframework.context.annotation.Bean;
 import java.util.Date;
 
 @SpringBootApplication
-
+@Slf4j
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+@Bean
+CommandLineRunner startCompte(OperationServiceImpl operationService,
+						OperationRepository operationRepository,
+						CompteService compteService) {
+	return args -> {
+		for (int i=0; i<10; i++) {
+			CompteDto compteDto = new CompteDto();
+			compteDto.setRibCompte(i+123456789L);
+			log.info("itération : " + i + "rib : " + compteDto.getRibCompte());
+			compteDto.setBalance(Math.random()*100000);
+			compteDto.setDevise("MAD");
+			compteDto.setDateCreation(new Date());
+			compteDto.setTypeCompte(TypeCompte.COMPTE_COURANT);
+			compteService.createCompte(compteDto);
+		}
+	};
+}
 
 
-   /* @Bean
-    CommandLineRunner start(OperationServiceImpl operationService,
-                            OperationRepository operationRepository,
-                            CompteService compteService) {
-        return args -> {
-            for (int i = 0; i < 10; i++) {
-                CompteDto compteDto = new CompteDto();
-                compteDto.setRibCompte(i + 123456789L);
-                compteDto.setBalance(Math.random() * 100000);
-                compteDto.setDevise("MAD");
-                compteDto.setDateCreation(new Date());
-                compteDto.setTypeCompte(TypeCompte.COMPTE_COURANT);
-                compteService.createCompte(compteDto);
-            }
-        };
-    }
-*/
 
-   @Bean
-    CommandLineRunner start(OperationServiceImpl operationService,
-                            OperationRepository operationRepository,
-                            CompteService compteService) {
-        return args -> {
-            for (int i = 0; i < 10; i++) {
-                CompteDto compteDto = new CompteDto();
-                OperationDto operationDto = new OperationDto();
-                operationDto.setRibCompte(123456789L);
-                operationDto.setMontant(Math.random() * 100000);
-                operationDto.setDateOperation(new Date());
-                operationDto.setTypeOperation(String.valueOf(TypeCompte.COMPTE_COURANT));
-                operationService.createOperation(operationDto);
+/*	@Bean
+	CommandLineRunner start(OperationServiceImpl operationService,
+							OperationRepository operationRepository,
+							CompteService compteService) {
+		return args -> {
+			for (int i=0; i<10; i++) {
+				CompteDto compteDto = new CompteDto();
+				OperationDto operationDto = new OperationDto();
+				operationDto.setRibCompte(123456789L);
+				operationDto.setMontant(Math.random()*100000);
+				operationDto.setDateOperation(new Date());
+				operationDto.setTypeOperation(String.valueOf(TypeCompte.COMPTE_COURANT));
+				operationService.createOperation(operationDto);
 
-            }
-        };
-    }
+			}
+		};
+	}*/
+
+
+
 
 
 }
--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/web/CompteController.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/web/CompteController.java
@@ -1,41 +1,43 @@
 package ma.atos.ma.atos.bankmanagement.web;
 
-
 import lombok.AllArgsConstructor;
 import ma.atos.ma.atos.bankmanagement.Dtos.CompteDto;
+import ma.atos.ma.atos.bankmanagement.Dtos.responses.GenericResponse;
 import ma.atos.ma.atos.bankmanagement.exceptions.CompteException;
-import ma.atos.ma.atos.bankmanagement.services.CompteService;
+import ma.atos.ma.atos.bankmanagement.services.CompteServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
 
-@AllArgsConstructor
 @RestController
+@AllArgsConstructor
 public class CompteController {
+    CompteServiceImpl compteService;
+    MessageSource messageSource;
 
-    CompteService compteService;
-
-
-    @GetMapping("comptes")
-    public ResponseEntity<List<CompteDto>> listComptes() throws CompteException {
-        List<CompteDto> compteDtoList = compteService.listComptes();
-        return new ResponseEntity<>(compteDtoList, HttpStatus.OK);
+    @GetMapping("/comptes")
+    public ResponseEntity<List<CompteDto>> listCompte() throws CompteException {
+        return ResponseEntity.ok(compteService.listComptes());
+    }
+    @GetMapping("/comptes/{ribCompte}")
+    public ResponseEntity<CompteDto> getCompte(@PathVariable Long ribCompte) {
+        return ResponseEntity.ok(compteService.getCompte(ribCompte));
     }
 
-    @DeleteMapping("delete/{ribCompte}")
-    public ResponseEntity<?> deleteCompte(@PathVariable("ribCompte") Long ribCompte) throws CompteException {
-        compteService.deleteCompte(ribCompte);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @PostMapping("/comptes/createCompte")
+    public ResponseEntity<GenericResponse> createCompte(@RequestBody CompteDto compteDto) {
+        GenericResponse result = new GenericResponse();
+        compteService.createCompte(compteDto);
+
+        result.setDescription(messageSource.getMessage("account.created.success", new Object[] {compteDto.getRibCompte()}, Locale.getDefault()));
+        result.setStatusCode(String.valueOf(HttpStatus.OK));
+
+        return ResponseEntity.ok(result);
     }
 
-    @GetMapping("comptesByTier/{numClient}")
-    public ResponseEntity<List<CompteDto>> listComptesByTier(@PathVariable("numClient") String numClient) throws CompteException {
-        List<CompteDto> compteDtoList = compteService.getCompteByTier(numClient);
-        return new ResponseEntity<>(compteDtoList, HttpStatus.OK);
-    }
 }
