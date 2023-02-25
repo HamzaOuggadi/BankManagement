89bc19f49329937d2b6c02c836e55468e949c8b0
HamzaOuggadi <hamza.ouggadi@gmail.com>
2023-02-25 15:45:01 +0100

    Added Exception Handling

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/exceptions/CompteException.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/exceptions/CompteException.java
@@ -2,6 +2,9 @@ package ma.atos.ma.atos.bankmanagement.exceptions;
 
 import lombok.Getter;
 import lombok.Setter;
+import ma.atos.ma.atos.bankmanagement.entities.Compte;
+import ma.atos.ma.atos.bankmanagement.enums.ApiStatusCode;
+import org.apache.logging.log4j.message.StringFormattedMessage;
 import org.springframework.http.HttpStatus;
 @Getter
 @Setter
@@ -9,23 +12,31 @@ public class CompteException extends Exception {
 
     private String message;
     private String messageFront;
-    private String code;
+    private ApiStatusCode code;
     private HttpStatus httpStatus;
     public CompteException(String message) {
         super();
         this.message = message;
     }
 
-    public CompteException(String message, String messageFront , String code) {
+    public CompteException(String message, String messageFront , ApiStatusCode code) {
         super();
         this.message = message;
         this.messageFront = messageFront;
         this.code = code;
     }
 
-    public CompteException(String message, String code, HttpStatus httpStatus) {
+    public CompteException(String message, ApiStatusCode code, HttpStatus httpStatus) {
         super();
         this.message = message;
+        this.code = code;
+        this.httpStatus = httpStatus;
+    }
+
+    public CompteException(String message, String messageFront, ApiStatusCode code, HttpStatus httpStatus) {
+        super();
+        this.message = message;
+        this.messageFront = messageFront;
         this.code = code;
         this.httpStatus = httpStatus;
     }
--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/services/CompteService.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/services/CompteService.java
@@ -3,14 +3,13 @@ package ma.atos.ma.atos.bankmanagement.services;
 import ma.atos.ma.atos.bankmanagement.Dtos.CompteDto;
 import ma.atos.ma.atos.bankmanagement.entities.Compte;
 import ma.atos.ma.atos.bankmanagement.exceptions.CompteException;
-import ma.atos.ma.atos.bankmanagement.exceptions.TierNotFoundExeption;
 
 import java.util.List;
 
 public interface CompteService {
     List<CompteDto> listComptes() throws CompteException;
     CompteDto getCompte(Long ribCompte) throws CompteException;
-    List<CompteDto> getCompteByTier(String numClient) throws CompteException, TierNotFoundExeption;
-    void createCompte(CompteDto compteDto) throws CompteException;
+    List<CompteDto> getCompteByTier(String numClient) throws CompteException;
+    void createCompte(CompteDto compteDto);
     void deleteCompte(Long ribCompte) throws CompteException;
 }
