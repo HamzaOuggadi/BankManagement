39d6249dbf83dcc6163ceb098fdd8ab51d023e6d
HamzaOuggadi <hamza.ouggadi@gmail.com>
2023-02-25 16:42:08 +0100

    Finalised Exception Handling for Compte

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/services/CompteService.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/services/CompteService.java
@@ -3,13 +3,14 @@ package ma.atos.ma.atos.bankmanagement.services;
 import ma.atos.ma.atos.bankmanagement.Dtos.CompteDto;
 import ma.atos.ma.atos.bankmanagement.entities.Compte;
 import ma.atos.ma.atos.bankmanagement.exceptions.CompteException;
+import ma.atos.ma.atos.bankmanagement.exceptions.TierNotFoundExeption;
 
 import java.util.List;
 
 public interface CompteService {
     List<CompteDto> listComptes() throws CompteException;
     CompteDto getCompte(Long ribCompte) throws CompteException;
-    List<CompteDto> getCompteByTier(String numClient) throws CompteException;
-    void createCompte(CompteDto compteDto);
+    List<CompteDto> getCompteByTier(String numClient) throws CompteException, TierNotFoundExeption;
+    void createCompte(CompteDto compteDto) throws CompteException;
     void deleteCompte(Long ribCompte) throws CompteException;
 }
