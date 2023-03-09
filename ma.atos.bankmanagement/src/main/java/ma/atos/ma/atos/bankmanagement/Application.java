5dccbe40414032982baf7f00730f55aeb67b728b
HamzaOuggadi <hamza.ouggadi@gmail.com>
2023-03-09 11:05:54 +0100

    Finalised Exception Handling for Compte

--- ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/Application.java
+++ ma.atos.bankmanagement/src/main/java/ma/atos/ma/atos/bankmanagement/Application.java
@@ -16,11 +16,15 @@ import ma.atos.ma.atos.bankmanagement.services.OperationServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
 @Slf4j
 public class Application {
 
